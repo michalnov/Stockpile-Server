0c97eeaf400dff1127fc3899a320c028f1ee7eaa
michal.novotny <michal.novotny@akademiasovy.sk>
2018-11-04 11:14:29 +0100

    ..

--- build/handler/authentication.go
+++ build/handler/authentication.go
@@ -8,20 +8,41 @@ import (
 	"time"
 )
 
+type user struct {
+	Username string
+	Token    string
+}
+
+var Users = make([]user, 0)
+
 const charset = "GHIJK456789abOPQRScdeUVmXYZfghijkLMNT0123lmnopYZ012qrsCDEtuv7xyzABF"
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	req := loginRequest{}
 	_ = json.NewDecoder(r.Body).Decode(&req)
 	if req.Username == "tester" && req.Password == "test123" {
 		res := loginRequest{}
 		res.Token = randomString(64)
+		Users = append(Users, user{Username: req.Username, Token: res.Token})
 		response := json.NewEncoder(w).Encode(res)
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, response)
 	} else {
+		w.WriteHeader(http.StatusForbidden)
 		fmt.Fprintf(w, "\"token\":\"\"")
 	}
 
+}
+
+func scanUsers(toekn string) bool {
+	for index, element := range Users {
+		if element.Token == toekn {
+			return true
+		}
+	}
+	return false
 }
 
 func randomString(lenght int) string {
--- build/handler/stock.go
+++ build/handler/stock.go
@@ -2,9 +2,13 @@ package handler
 
 import (
 	"database/sql"
+	"encoding/json"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var dbconf string = "root:sql@tcp(127.0.0.1:3306)/michalstock"
 
 func notImplemented() {
 
@@ -19,9 +23,33 @@ type StockUnit struct {
 }
 
 type StockList struct {
-	Stockunits StockUnit `json:"stockunits,omitempty"`
+	Stockunits []StockUnit `json:"stockunits,omitempty"`
 }
 
 func UpdateStock(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var res StockList
+
+	req := loginRequest{}
+	_ = json.NewDecoder(r.Body).Decode(&req)
+	if req.Token == "" {
+		w.WriteHeader(http.StatusForbidden)
+		response := json.NewEncoder(w).Encode(res)
+		fmt.Fprintf(w, response)
+	} else if !scanUsers(req.Token) {
+		w.WriteHeader(http.StatusForbidden)
+		response := json.NewEncoder(w).Encode(res)
+		fmt.Fprintf(w, response)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		db, err := sql.Open("mysql", dbconf)
+		if err != nil {
+			fmt.Fprintf(w, "{\"Error\": 500}")
+			panic(err.Error())
+		}
+
+		response := json.NewEncoder(w).Encode(res)
+		fmt.Fprintf(w, response)
+	}
 
 }
